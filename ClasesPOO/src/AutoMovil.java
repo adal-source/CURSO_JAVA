ba943171ef81b1fc356cd21163e3fa657ea0431c
adal-source <asalinasjoses@gmail.com>
2025-05-17 17:33:18 -0600

    Leccion 141

--- ClasesPOO/src/AutoMovil.java
+++ ClasesPOO/src/AutoMovil.java
@@ -1,7 +1,133 @@
 public class AutoMovil {
 
-    String fabricante;
-    String modelo;
-    String color;
-    double cilindrada;
+    private String fabricante;
+    private String modelo;
+    private String color;
+    private double cilindrada;
+    private int capacidadTanque = 40;
+
+    // ejemplo de un constructor
+    /// no es un miembro de la clase y es void por defecto aun que no se pone
+    /*
+    public AutoMovil(String fabricante, String modelo){
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+    }
+
+    public AutoMovil(String fabricante, String modelo){
+        // forma de llamar al constructor de esta manera se ahorra codigo o se optimiza
+        this.(fabricante, modelo);
+        this.color = color;
+    }
+    */
+
+    public String getFabricante(){
+        return this.fabricante;
+    }
+    public void setFabricante(String fabricante){
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo(){
+        return this.fabricante;
+    }
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public double getCilindrada(){
+        return this.cilindrada;
+    }
+    public void setCilindrada(double cilindrada){
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCapacidadTanque(){
+        return this.capacidadTanque;
+    }
+    public void setCapacidadTanque(int capacidadTanque){
+        this.capacidadTanque = capacidadTanque;
+    }
+
+    /// funciones / comportamientos
+    public String detalle(){ //verDetalle
+        /// por defecto se asume que es atributo de la clase
+
+        String sb = "this.fabricante = " + this.fabricante +
+                "\n this.modelo = " + this.modelo +
+                "\n this.color = " + this.color +
+                "\n this.cilindrada = " + this.cilindrada;
+        return sb;
+
+        /*
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("this.fabricante = " + this.fabricante);
+        sb.append("\n this.modelo = " + this.modelo);
+        sb.append("\n this.color = " + this.color);
+        sb.append("\n this.cilindrada = " + this.cilindrada);
+
+        System.out.println("this.fabricante = " + this.fabricante);
+        System.out.println("this.modelo = " + this.modelo);
+        System.out.println("this.color = " + this.color);
+        System.out.println("this.cilindrada = " + this.cilindrada);
+        */
+    }
+
+    public String acelerar(int rpm){
+        return "el auto " + fabricante + " acelerando a " + rpm + " rmp";
+
+    }
+
+    public String frenar(){
+        return "el auto " + fabricante + " frenando!!";
+    }
+
+    public String acelerarFrenar(int rpm){
+        String acelerar = this.acelerar(rpm);
+        String frenar = this.frenar();
+
+        return acelerar + "\n" + frenar;
+    }
+
+    public float calcularConsumo(int km, float porcentajeBencina){
+        return km/(capacidadTanque*porcentajeBencina);
+    }
+
+    /// con el mismo nombre se conce como sobre carga de metodos
+    public float calcularConsumo(int km, int porcentajeBencina){
+        /// se debe poner f para que el resultado sea en punto flotante y que no se redondee
+        return km/(capacidadTanque*(porcentajeBencina/100f));
+    }
+
+    /// sobre escribiendo metodo del padre
+    @Override
+    public boolean equals(Object obj) {
+        AutoMovil a = (AutoMovil) obj;
+        /*
+        // si es el mismo objeto no sera necesario comparar
+            if (this == obj){
+            return true;
+            }
+        * */
+
+        // se indica que si no es una estancia de automovil que por defecto
+        // sea false, en lugar de dar error que indica java
+        if (!(obj instanceof AutoMovil)) {
+            return false;
+        }
+        // con esto se valida si los valores son nulos
+        return (this.fabricante != null && this.modelo != null
+                &&this.fabricante.equals(a.getFabricante())
+                && this.modelo.equals(a.getModelo()));
+    }
 }
+
--- ClasesPOO/src/EjemploAutoMovil.java
+++ ClasesPOO/src/EjemploAutoMovil.java
@@ -3,26 +3,53 @@ public class EjemploAutoMovil {
         /// funciones que porcesan datos
 
         /// clase            constructor
+        //AutoMovil auto = new AutoMovil("impresa","subaru" );
         AutoMovil auto = new AutoMovil();
-        auto.fabricante = "impresa";
-        auto.modelo = "subaru";
-        auto.color = "blanco";
-        auto.cilindrada = 2.0;
+        auto.setFabricante("impresa");
+        auto.setModelo("subaru");
+        auto.setColor("blanco");
+        auto.setCilindrada(2.0);
 
+        //AutoMovil mazda = new AutoMovil("mazda","bt-50");
         AutoMovil mazda = new AutoMovil();
-        mazda.fabricante = "mazda";
-        mazda.modelo = "bt-50";
-        mazda.color = "rojo";
-        mazda.cilindrada = 3.0;
+        mazda.setFabricante("mazda");
+        mazda.setModelo("bt-50");
+        mazda.setColor("rojo");
+        mazda.setCilindrada(3.0);
 
-        System.out.println("auto.fabricante = " + auto.fabricante);
-        System.out.println("auto.modelo = " + auto.modelo);
-        System.out.println("auto.color = " + auto.color);
-        System.out.println("auto.cilindrada = " + auto.cilindrada);
+        AutoMovil nissan = new AutoMovil();
+        nissan.setFabricante("nissan");
+        nissan.setModelo("navara");
+        nissan.setColor("gris oscuro");
+        nissan.setCilindrada(3.5);
+        nissan.setCapacidadTanque(50);
 
-        System.out.println("mazda.fabricante = " + mazda.fabricante);
-        System.out.println("mazda.modelo = " + mazda.modelo);
-        System.out.println("mazda.color = " + mazda.color);
-        System.out.println("mazda.cilindrada = " + mazda.cilindrada);
+        AutoMovil nissan2 = new AutoMovil();
+        nissan2.setFabricante("nissan");
+        nissan2.setModelo("navara");
+        nissan2.setColor("gris oscuro");
+        nissan2.setCilindrada(3.5);
+        nissan2.setCapacidadTanque(50);
+
+        System.out.println("son giales con == " + (nissan == nissan2));
+        System.out.println("son giales con equals " + (nissan.equals(nissan2)));
+
+        /// solo se llama el metodo dentro de automovil
+        /// para imprimir todo
+        System.out.println(auto.getFabricante());
+        //auto.detalle();
+        System.out.println(auto.detalle());
+        System.out.println();
+        //mazda.detalle();
+        System.out.println(mazda.detalle());
+        System.out.println(auto.acelerar(3000));
+        System.out.println(auto.frenar());
+
+        System.out.println();
+        System.out.println(auto.acelerarFrenar(4000));
+
+        System.out.println("kilometros por litro " + auto.calcularConsumo(300, 0.6f));
+        System.out.println("kilometros por litro " + auto.calcularConsumo(300, 60));
+
     }
 }
